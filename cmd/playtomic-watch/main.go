9b39b407a2bbae259e6e5501af97727a8ed9dee6
Taras <taras.sologub@pm.me>
2026-04-09 11:22:35 +0200

    Fix formatting of tenantNames map

--- cmd/playtomic-watch/main.go
+++ cmd/playtomic-watch/main.go
@@ -237,7 +237,7 @@ func main() {
 // tenantNames maps known tenant IDs to human-readable club names.
 var tenantNames = map[string]string{
 	"8b818dae-aacb-4ea3-aa7b-0e77b1149c85": "Charlotte-Mitte",
-	"9fea856e-7d1a-4cae-9831-79015318967b": "PBC"
+	"9fea856e-7d1a-4cae-9831-79015318967b": "PBC",
 }
 
 func tenantName(id string) string {
